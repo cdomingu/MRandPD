a1ea247fcc124907fc9ba4178eced3f9c54a0b04
Carmen Domínguez <53055793+cdomingu@users.noreply.github.com>
2019-08-29 09:28:11 -0400

    Update Smoking_Clumping.sh

--- Smoking_Clumping.sh
+++ Smoking_Clumping.sh
@@ -5,27 +5,26 @@
 
 #2. PREPARE GWAS FILES FOR SNP CLUMPING (in PLINK)
 # EXTRACTION OF COLUMNS (SNP CHR BP P) from the GWAS results
+mkdir /data/neurogen/MRandPD/Results/PLINKClumping
+mkdir /data/neurogen/MRandPD/Results/PLINKClumping/GWASData4Clumping
 cd /data/neurogen/MRandPD/GWASsummaryStats/Smoking2019/
 echo "SNP CHR BP P" | sed -e 's/ /\t/g'  > header
-awk '{ if (NR!=1) print $3,$1,$2,$8}' CigarettesPerDay.txt | sed -e 's/ /\t/g' > temp
-cat header temp > /data/neurogen/MRandPD/Results/Clumping/Data4Clump/cigarettespday
-awk '{ if (NR!=1) print $3,$1,$2,$8}' EverSmoking.txt | sed -e 's/ /\t/g' > temp
-cat header temp > /data/neurogen/MRandPD/Results/Clumping/Data4Clump/eversmoking
-awk '{ if (NR!=1) print $3,$1,$2,$8}' SmokingStatus.txt | sed -e 's/ /\t/g' > temp
-cat header temp > /data/neurogen/MRandPD/Results/Clumping/Data4Clump/smokingstatus
-awk '{ if (NR!=1) print $3,$1,$2,$8}' AgeOfInitiation.txt | sed -e 's/ /\t/g' > temp
-cat header temp > /data/neurogen/MRandPD/Results/Clumping/Data4Clump/ageofinitiation
+for i in CigarettesPerDay EverSmoking SmokingStatus AgeOfInitiation
+do 
+awk '{ if (NR!=1) print $3,$1,$2,$8}' $i.txt | sed -e 's/ /\t/g' > temp
+cat header temp > /data/neurogen/MRandPD/Results/PLINKClumping/GWASData4Clumping/$i
+done
 
 # 3.1 COLLATE ALL SNPS FROM GWAS
-cd /data/neurogen/MRandPD/Results/Clumping/Data4Clump/
-for i in cigarettespday eversmoking smokingstatus ageofinitiation
+cd /data/neurogen/MRandPD/Results/PLINKClumping/GWASData4Clumping/
+for i in CigarettesPerDay EverSmoking SmokingStatus AgeOfInitiation
 do echo $i
-awk '{ if (NR != 1) print $1}' $i >> allSnpRsGWASsmoking2019
+awk '{ if (NR != 1) print $1}' $i >> allSnpRsGWASSmoking2019
 done
 # 3.3 SORT RS NUMBERS AND REMOVE DUPLICATED SNPS
-sort allSnpRsGWASsmoking2019 | uniq > allSnpRsUniquesmoking2019
+sort allSnpRsGWASSmoking2019 | uniq > allSnpRsUniqueSmoking2019
 # 3.4 GET RID OF INDELS (THEY DO NOT HAVE AN RS NUMBER)
-grep rs allSnpRsUniquesmoking2019 > allSnpRsUniqueNoIndelssmoking2019
+grep rs allSnpRsUniqueSmoking2019 > allSnpRsUniqueNoIndelsSmoking2019
 
 # We will be ussing the 1000Genomes data in Plink format downloaded from: http://www.cog-genomics.org/plink/1.9/resources for clumping
 
@@ -33,20 +32,23 @@ grep rs allSnpRsUniquesmoking2019 > allSnpRsUniqueNoIndelssmoking2019
 #Parameters for clumping: --clump-p1 .00000005 --clump-p2 .01 --clump-r2 0.001 --clump-kb 10000 --clump-verbose
 #this mean that index snps will reach genomewide significance (.00000005) and either have a r2=0.001 with the next index SNP or be 10000 kb appart
 #--clump-verbose to ger the Rsquare values of the clumped SNPs with the index SNP
-for i in cigarettespday eversmoking smokingstatus ageofinitiation
+mkdir /data/neurogen/MRandPD/Scripts/PLINKClumpScripts/
+mkdir /data/neurogen/MRandPD/Scripts/PLINKClumpScripts/Smoking2019ScrptClumpPmin8V/
+mkdir /data/neurogen/MRandPD/Results/PLINKClumping/Smoking2019ClumpPmin8V/
+for i in CigarettesPerDay EverSmoking SmokingStatus AgeOfInitiation
 do
 for j in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22
 do
-cp /data/neurogen/MRandPD/Scripts/test.lsf /data/neurogen/MRandPD/Scripts/Clmp1000GPlnkFrmt/Smoking2019ScrptClumppvalmin8Verbose 
-mv /data/neurogen/MRandPD/Scripts/Clmp1000GPlnkFrmt/Smoking2019ScrptClumppvalmin8Verbose/test.lsf /data/neurogen/MRandPD/Scripts/Clmp1000GPlnkFrmt/Smoking2019ScrptClumppvalmin8Verbose/"$i".clump_chr"$j".lsf
-echo "module load plink/1.90b3" >> /data/neurogen/MRandPD/Scripts/Clmp1000GPlnkFrmt/Smoking2019ScrptClumppvalmin8Verbose/"$i".clump_chr"$j".lsf
-echo "plink --bfile /data/neurogen/MRandPD/1000GenomsPlinkFormat/1kg_phase1_chr"$j" --extract /data/neurogen/MRandPD/Results/Clumping/Data4Clump/allSnpRsUniqueNoIndelssmoking2019 --clump /data/neurogen/MRandPD/Results/Clumping/Data4Clump/"$i" --clump-p1 .00000005 --clump-p2 .01 --clump-r2 0.001 --clump-kb 10000 --clump-verbose --out /data/neurogen/MRandPD/Results/Clumping/Smoking2019Clumppvalmin8Verbose/clumped."$i".chr"$j" " >> /data/neurogen/MRandPD/Scripts/Clmp1000GPlnkFrmt/Smoking2019ScrptClumppvalmin8Verbose/"$i".clump_chr"$j".lsf
+cp /data/neurogen/MRandPD/Scripts/test.lsf /data/neurogen/MRandPD/Scripts/PLINKClumpScripts/Smoking2019ScrptClumpPmin8V 
+mv /data/neurogen/MRandPD/Scripts/PLINKClumpScripts/Smoking2019ScrptClumpPmin8V/test.lsf /data/neurogen/MRandPD/Scripts/PLINKClumpScripts/Smoking2019ScrptClumpPmin8V/"$i".clump_chr"$j".lsf
+echo "module load plink/1.90b3" >> /data/neurogen/MRandPD/Scripts/PLINKClumpScripts/Smoking2019ScrptClumpPmin8V/"$i".clump_chr"$j".lsf
+echo "plink --bfile /data/neurogen/MRandPD/1000GenomsPlinkFormat/1kg_phase1_chr"$j" --extract /data/neurogen/MRandPD/Results/PLINKClumping/GWASData4Clumping/allSnpRsUniqueNoIndelsSmoking2019 --clump /data/neurogen/MRandPD/Results/PLINKClumping/GWASData4Clumping/"$i" --clump-p1 .00000005 --clump-p2 .01 --clump-r2 0.001 --clump-kb 10000 --clump-verbose --out /data/neurogen/MRandPD/Results/PLINKClumping/Smoking2019ClumpPmin8V/clumped."$i".chr"$j" " >> /data/neurogen/MRandPD/Scripts/PLINKClumpScripts/Smoking2019ScrptClumpPmin8V/"$i".clump_chr"$j".lsf
 done
 done
 
 # SUBMIT THEM TO THE QUEUE
-cd /data/neurogen/MRandPD/Scripts/Clmp1000GPlnkFrmt/Smoking2019ScrptClumppvalmin8Verbose/
-for i in cigarettespday eversmoking smokingstatus ageofinitiation
+cd /data/neurogen/MRandPD/Scripts/PLINKClumpScripts/Smoking2019ScrptClumpPmin8V/
+for i in CigarettesPerDay EverSmoking SmokingStatus AgeOfInitiation
 do
 for j in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22
 do
@@ -54,70 +56,51 @@ bsub -q normal < "$i".clump_chr"$j".lsf
 done 
 done
 
-#5.EXTRACT THE LIST OF INDEXSNPS FROM CLUMPING RESULTS
-cd /data/neurogen/MRandPD/Results/Clumping/Smoking2019Clumppvalmin8Verbose/
-cat clumped.cigarettespday.chr*.clumped > temp
-grep 'INDEX' temp | sed -e 's/    / /g' | sed -e 's/   / /g' | sed -e 's/  / /g' | cut -d ' ' -f3 > indexSNPScigperdayverb
-cat clumped.eversmoking.chr*.clumped > temp
-grep 'INDEX' temp | sed -e 's/    / /g' | sed -e 's/   / /g' | sed -e 's/  / /g' | cut -d ' ' -f3 > indexSNPSeversmokverb
-cat clumped.smokingstatus.chr*.clumped > temp
-grep 'INDEX' temp | sed -e 's/    / /g' | sed -e 's/   / /g' | sed -e 's/  / /g' | cut -d ' ' -f3 > indexSNPSsmokingstat
-cat clumped.ageofinitiation.chr*.clumped > temp
-grep 'INDEX' temp | sed -e 's/    / /g' | sed -e 's/   / /g' | sed -e 's/  / /g' | cut -d ' ' -f3 > indexSNPSageofinit
+#Verify that all clumping was compleated. to do so, run the following command and it whould return 88. if not, look for the ones missing and rerun them 
+cd /data/neurogen/MRandPD/Results/PLINKClumping/Smoking2019ClumpPmin8V/
+ls | grep 'log' | wc -l
 
-#6.EXTRACT EXPOSURE GWAS INFO (specially BETA, MAF,SE) FOR CALCUATING PHENOTYPIC VARIANCE EXPALINED (Rsquare) AND F-STATISTIC
+#5.EXTRACT THE LIST OF INDEX SNPS FROM CLUMPING RESULTS
+for i in CigarettesPerDay EverSmoking SmokingStatus AgeOfInitiation
+do
+cat clumped.$i.chr*.clumped > temp
+grep 'INDEX' temp | sed -e 's/    / /g' | sed -e 's/   / /g' | sed -e 's/  / /g' | cut -d ' ' -f3 > indexSNPs"$i"V
+done
+
+#6.EXTRACT EXPOSURE GWAS INFO FOR CALCUATING PHENOTYPIC VARIANCE EXPALINED (Rsquare) AND F-STATISTIC
+mkdir /data/neurogen/MRandPD/Results/R2andFstat4IndexSNPs/
 echo "CHROM POS RSID REF ALT AF STAT PVALUE BETA SE N EFFECTIVE_N Number_of_Studies ANNO ANNOFULL" | sed -e 's/ /\t/g'  > header
-grep -w -f indexSNPSageofinit /data/neurogen/MRandPD/GWASsummaryStats/Smoking2019/AgeOfInitiation.txt | sort -n -k 2 | uniq > temp
-cat header temp | sed -e 's/ /\t/g' > /data/neurogen/MRandPD/Results/R2andFstatisticprePD/ageofinitiationforR2calc
-grep -w -f indexSNPScigperdayverb /data/neurogen/MRandPD/GWASsummaryStats/Smoking2019/CigarettesPerDay.txt | sort -n -k 2 | uniq > temp
-cat header temp | sed -e 's/ /\t/g' > /data/neurogen/MRandPD/Results/R2andFstatisticprePD/cigarettespdayforR2calc
-grep -w -f indexSNPSeversmokverb /data/neurogen/MRandPD/GWASsummaryStats/Smoking2019/EverSmoking.txt | sort -n -k 2 | uniq > temp
-cat header temp | sed -e 's/ /\t/g' > /data/neurogen/MRandPD/Results/R2andFstatisticprePD/eversmokingforR2calc
-grep -w -f indexSNPSsmokingstat /data/neurogen/MRandPD/GWASsummaryStats/Smoking2019/SmokingStatus.txt | sort -n -k 2 | uniq > temp
-cat header temp | sed -e 's/ /\t/g' > /data/neurogen/MRandPD/Results/R2andFstatisticprePD/smokingstatusforR2calc
+for i in CigarettesPerDay EverSmoking SmokingStatus AgeOfInitiation
+do
+grep -w -f indexSNPs"$i"V /data/neurogen/MRandPD/GWASsummaryStats/Smoking2019/$i.txt | sort -n -k 2 | uniq > temp
+cat header temp | sed -e 's/ /\t/g' > /data/neurogen/MRandPD/Results/R2andFstat4IndexSNPs/"$i"forR2calc
+done
 
 #7. AS PD DATASET DO NOT HAVE rsID BUT CHR:BP VALUES, EXTRACT THE CHR:POS OF OUR INDEX SNPS
-awk '{ if (NR!=1) print $1,$2}' ageofinitiationforR2calc > temp
-sed -e 's/^/chr/g' temp | sed -e 's/ /:/g' > ageofinitchrpos
-awk '{ if (NR!=1) print $1,$2}' cigarettespdayforR2calc > temp
-sed -e 's/^/chr/g' temp | sed -e 's/ /:/g' > cigarettespdaychrpos
-awk '{ if (NR!=1) print $1,$2}' eversmokingforR2calc > temp
-sed -e 's/^/chr/g' temp | sed -e 's/ /:/g' > eversmokingchrpos
-awk '{ if (NR!=1) print $1,$2}' smokingstatusforR2calc > temp
-sed -e 's/^/chr/g' temp | sed -e 's/ /:/g' > smokingstatchrpos
+cd /data/neurogen/MRandPD/Results/R2andFstat4IndexSNPs/
+echo "SNP rsID" > header2
+for i in CigarettesPerDay EverSmoking SmokingStatus AgeOfInitiation
+do
+awk '{ if (NR!=1) print $3}' "$i"forR2calc > "$i"rsID
+awk '{ if (NR!=1) print $1,$2}' "$i"forR2calc > temp
+sed -e 's/^/chr/g' temp | sed -e 's/ /:/g' > "$i"chrpos
+paste "$i"chrpos "$i"rsID | sort > temp2
+cat header2 temp2 | sed -e 's/ /\t/g' > "$i"chrposrsID
+done
 
-awk '{ if (NR!=1) print $3}' cigarettespdayforR2calc > cigarettespdayrsID
-paste cigarettespdaychrpos cigarettespdayrsID | sort > temp
-cat header2 temp | sed -e 's/ /\t/g' > cigarettespdaychrposrsID
-awk '{ if (NR!=1) print $3}' drinksperweekforR2calc > drinkspweekrsID
-paste drinkspweekchrpos drinkspweekrsID | sort > temp
-cat header2 temp | sed -e 's/ /\t/g' > drinkspweekchrposrsID
-awk '{ if (NR!=1) print $3}' eversmokingforR2calc > eversmokingrsID
-paste eversmokingchrpos eversmokingrsID | sort > temp
-cat header2 temp | sed -e 's/ /\t/g' > eversmokingposrsID
-awk '{ if (NR!=1) print $3}' smokingstatusforR2calc > smokingstatrsID
-paste smokingstatchrpos smokingstatrsID | sort > temp
-cat header2 temp | sed -e 's/ /\t/g' > smokingstatposrsID
-
-#7. EXTRACT THE INFORMATION OF THE INDEX SNPS BUT FROM THE PD GWAS DATASET
+#8. EXTRACT THE INFORMATION OF THE INDEX SNPS BUT FROM THE PD GWAS DATASET
+mkdir /data/neurogen/MRandPD/Results/HarmonizationNallsPDdata/
 echo "SNP A1 A2 freq b se p N_cases N_controls" > header
-grep -w -f ageofinitchrpos /data/neurogen/MRandPD/GWASsummaryStats/PDnalls2019/nallsEtAl2019_excluding23andMe_allVariants.tab| sort | uniq > temp
-cat header temp | sed -e 's/ /\t/g' > /data/neurogen/MRandPD/Results/HarmonizationNalls2019/ageofinitSNPinNallsPDdataV
-grep -w -f cigarettespdaychrpos /data/neurogen/MRandPD/GWASsummaryStats/PDnalls2019/nallsEtAl2019_excluding23andMe_allVariants.tab | sort | uniq > temp
-cat header temp | sed -e 's/ /\t/g' > /data/neurogen/MRandPD/Results/HarmonizationNalls2019/cigarettespdaySNPinNallsPDdataV
-grep -w -f eversmokingchrpos /data/neurogen/MRandPD/GWASsummaryStats/PDnalls2019/nallsEtAl2019_excluding23andMe_allVariants.tab| sort | uniq > temp
-cat header temp | sed -e 's/ /\t/g' > /data/neurogen/MRandPD/Results/HarmonizationNalls2019/eversmokingSNPinNallsPDdataV
-grep -w -f smokingstatchrpos /data/neurogen/MRandPD/GWASsummaryStats/PDnalls2019/nallsEtAl2019_excluding23andMe_allVariants.tab | sort | uniq > temp
-cat header temp | sed -e 's/ /\t/g' > /data/neurogen/MRandPD/Results/HarmonizationNalls2019/smokingstatSNPinNallsPDdataV
+for i in CigarettesPerDay EverSmoking SmokingStatus AgeOfInitiation
+do
+grep -w -f "$i"chrpos /data/neurogen/MRandPD/GWASsummaryStats/PDnalls2019/nallsEtAl2019_excluding23andMe_allVariants.tab| sort | uniq > temp
+cat header temp | sed -e 's/ /\t/g' > /data/neurogen/MRandPD/Results/HarmonizationNallsPDdata/"$i"SNPsinNallsPDdataV
+done 
 
-#AD THE rsID NUMBER TO THE DATA
-join ageofinitSNPinNallsPDdataV /data/neurogen/MRandPD/Results/R2andFstatisticprePD/ageofinitchrposrsID | sed -e 's/ /\t/g' > temp
-mv temp ageofinitSNPinNallsPDdataV
-join cigarettespdaySNPinNallsPDdataV /data/neurogen/MRandPD/Results/R2andFstatisticprePD/cigarettespdaychrposrsID | sed -e 's/ /\t/g' > temp
-mv temp cigarettespdaySNPinNallsPDdataV
-join drinkspweekSNPinNallsPDdataV /data/neurogen/MRandPD/Results/R2andFstatisticprePD/drinkspweekchrposrsID | sed -e 's/ /\t/g' > temp
-mv temp drinkspweekSNPinNallsPDdataV
-join eversmokingSNPinNallsPDdataV /data/neurogen/MRandPD/Results/R2andFstatisticprePD/eversmokingposrsID | sed -e 's/ /\t/g' > temp
-mv temp eversmokingSNPinNallsPDdataV
-join smokingstatSNPinNallsPDdataV /data/neurogen/MRandPD/Results/R2andFstatisticprePD/smokingstatposrsID | sed -e 's/ /\t/g' > temp
-mv temp smokingstatSNPinNallsPDdataV
+#9. ADD THE rsID NUMBER TO THE PD DATA
+cd /data/neurogen/MRandPD/Results/HarmonizationNallsPDdata/
+for i in CigarettesPerDay EverSmoking SmokingStatus AgeOfInitiation
+do
+join "$i"SNPsinNallsPDdataV /data/neurogen/MRandPD/Results/R2andFstat4IndexSNPs/"$i"chrposrsID | sed -e 's/ /\t/g' > temp
+mv temp "$i"SNPsinNallsPDdataV
+done 
